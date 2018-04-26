971a47af509e36852bc4ad55a8ad9fb13bd9c6f5
moxsly <sviaroukin@gmail.com>
2018-04-26 16:18:32 +0300

    Add models and DbContext

---
+++ SelfControlDiary/SelfControlDiary/Models/IndicatorList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfControlDiary.Models
+{
+    public class IndicatorList
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public int Semestr { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public int JEL { get; set; }
+        public int BaseForce { get; set; }
+        public int LeftWrist { get; set; }
+        public int RightWrist { get; set; }
+        public int Pause { get; set; }
+        public int Breath { get; set; }
+        public int Exhalation { get; set; }
+        public int ChSS { get; set; }
+        public int ADS { get; set; }
+        public int ADD { get; set; }
+        public int Run { get; set; }
+        public int Incline { get; set; }
+        public int Bending { get; set; }
+        public int? Pulling { get; set; }
+        public int Squatting { get; set; }
+        public int? Press { get; set; }
+        public int Genchi { get; set; }
+        public int Shtange { get; set; }
+        public int CCC { get; set; }
+        public int Stat { get; set; }
+
+
+        public Student Student { get; set; }
+    }
+}
