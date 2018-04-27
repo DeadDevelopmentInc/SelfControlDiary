e39c12a7560c422f3868f0c65645a67c792c46d5
moxsly <sviaroukin@gmail.com>
2018-04-27 17:20:44 +0300

    Fix Home Redirect

--- SelfControlDiary/SelfControlDiary/Controllers/HomeController.cs
+++ SelfControlDiary/SelfControlDiary/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SelfControlDiary.Models;
 
@@ -10,8 +12,31 @@ namespace SelfControlDiary.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public HomeController(UserManager<User> userManager, SignInManager<User> signInManager,
+            RoleManager<IdentityRole> roleManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+    public async Task<IActionResult> Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                if (await _userManager.IsInRoleAsync(await _userManager.GetUserAsync(User), "admin"))
+                {
+                    return RedirectToAction("Index", "Students");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "LK");
+                }
+            }
             return View();
         }
 
